81cc68f2dac846caa18ec1e49ad23bd5c804d728
amiralaeifar <amplusir@gmail.com>
2023-07-12 12:52:19 +0330

    validators

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 ### powerful and flexible toolkit for building Web APIs
 
+_project overview:_
+
++ __serializer__ for user registration
++added all three validators to serializer: __field-level__, __object-level__, and __validators__ func
+
 
--- drf/accounts/serializers.py
+++ drf/accounts/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+
+def clean_email(value):
+    user = User.objects.filter(email=value).exists()
+    if user:
+        raise serializers.ValidationError('there is an account with this email, please enter a new one')
+    return value
 
 
 class UserRegisterSeralizer(serializers.Serializer):
     username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField(required=True, validators=[clean_email])
     password = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+    
+    
+    def validate_username(self, value):
+        user = User.objects.filter(username=value).exists()
+        if user:
+            raise serializers.ValidationError('username must be unique')
+        return value
+    
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('password must match')
+        return data
     
