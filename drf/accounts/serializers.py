853a788c14ddb985e3a9558b1d343911f8f6570f
amiralaeifar <amplusir@gmail.com>
2023-07-12 13:14:52 +0330

    ModelSerializer

--- drf/accounts/serializers.py
+++ drf/accounts/serializers.py
@@ -10,11 +10,16 @@ def clean_email(value):
     return value
 
 
-class UserRegisterSeralizer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True, validators=[clean_email])
-    password = serializers.CharField(required=True, write_only=True)
+class UserRegisterSeralizer(serializers.ModelSerializer):
     password2 = serializers.CharField(required=True, write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'validators': [clean_email,]},
+        }
     
     
     def validate_username(self, value):
