9b0db827971f99206060a90683711a3834c7f47d
amiralaeifar <amplusir@gmail.com>
2023-07-16 02:06:26 +0330

    SimpleJWT authentication

--- drf/accounts/urls.py
+++ drf/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path 
 from . import views 
-from rest_framework.authtoken import views as auth_token
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 
@@ -9,7 +9,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.UserRegister.as_view()),
-    path('api-token-auth/', auth_token.obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
 
 
